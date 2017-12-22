7dbb4c003dce7151887954ca1fd578e68dd44fc6
wang-emily <emily.wang7157@gmail.com>
2017-12-21 23:41:55 -0800

    Add files via upload

---
+++ biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+number = gets.chomp.to_i
+number = number + 1
+puts number.to_s + " is a bigger better favorite number!"
---
+++ fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "Hello, what is your first name?"
+firstname = gets.chomp
+puts "What is your middle name?"
+middlename = gets.chomp
+puts "What is your last name?"
+lastname = gets.chomp
+puts 'Pleased to meet you, ' + firstname + ' ' + middlename + ' ' + lastname + '!'
